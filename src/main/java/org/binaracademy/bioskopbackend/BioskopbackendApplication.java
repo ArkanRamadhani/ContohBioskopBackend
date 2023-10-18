861c20b1a3834f0a1219273fd59e2b5f023ec0be
Challenge_2 <arkanrd0412@gmail.com>
2023-10-18 21:05:59 +0700

    project terbaru

---
+++ src/main/java/org/binaracademy/bioskopbackend/BioskopbackendApplication.java
@@ -0,0 +1,13 @@
+package org.binaracademy.bioskopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioskopbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioskopbackendApplication.class, args);
+	}
+
+}
